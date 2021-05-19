3f4b99c445ad4d1797757357c6fe19a5ab6cc06d
brian dominguez <briandv198@gmail.com>
2021-05-19 18:34:40 -0500

    Aplicando estilos

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,26 +1,28 @@
 import React from "react";
 import confLogo from "../assets/images/badge-header.svg";
+import "../styles/badge.css";
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="badge">
+        <div className="badge__header">
           <img src={confLogo} alt="Logo conferencia"></img>
         </div>
 
-        <div>
-          <img src="" alt="avatar"></img>
+        <div className="badge__section-name">
+          <img src="" alt="avatar" className="section-name--avatar"></img>
           <h1>
             Brian <br /> Dominguez
           </h1>
         </div>
 
-        <div>
+        <div className="badge__section-info">
           <p>Web Developer</p>
           <p>@briandv98</p>
         </div>
-        <div>
+
+        <div className="badge__footer">
           <p>#conferencia</p>
         </div>
       </div>
