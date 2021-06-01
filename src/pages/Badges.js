7cc02ee09bc38eaa36ed4d1c6fcd8b62dd89f39f
brian dominguez <briandv198@gmail.com>
2021-06-01 10:00:02 -0500

    Solicitando datos Get

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import BadgesList from "../components/BadgesList.js";
 import MainButton from "../components/MainButton.js";
@@ -6,7 +7,7 @@ import MainButton from "../components/MainButton.js";
 import confLogo from "../assets/images/badge-header.svg";
 import "./styles/badges.css";
 
-import { Link } from "react-router-dom";
+import api from "../api";
 
 class Badges extends React.Component {
   constructor(props) {
@@ -14,49 +15,25 @@ class Badges extends React.Component {
     console.log("1: constructor");
 
     this.state = {
-      data: [],
+      loading: true,
+      error: null,
+      data: undefined,
     };
   }
   componentDidMount() {
     console.log("3: componentDidMount");
-
-    this.timeoutId = setTimeout(() => {
-      this.setState({
-        data: [
-          {
-            id: "2de30c42-9deb-40fc-a41f-05e62b5939a7",
-            firstName: "Freda",
-            lastName: "Grady",
-            email: "Leann_Berge@gmail.com",
-            jobTitle: "Legacy Brand Director",
-            twitter: "FredaGrady22221-7573",
-            avatarUrl:
-              "https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon",
-          },
-          {
-            id: "d00d3614-101a-44ca-b6c2-0be075aeed3d",
-            firstName: "Major",
-            lastName: "Rodriguez",
-            email: "Ilene66@hotmail.com",
-            jobTitle: "Human Research Architect",
-            twitter: "ajorRodriguez61545",
-            avatarUrl:
-              "https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon",
-          },
-          {
-            id: "63c03386-33a2-4512-9ac1-354ad7bec5e9",
-            firstName: "Daphney",
-            lastName: "Torphy",
-            email: "Ron61@hotmail.com",
-            jobTitle: "National Markets Officer",
-            twitter: "DaphneyTorphy96105",
-            avatarUrl:
-              "https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon",
-          },
-        ],
-      });
-    }, 3000);
+    this.fetchData();
   }
+
+  fetchData = async () => {
+    this.setState({ loading: true, error: null });
+
+    try {
+      const data = await api.badges.list();
+      this.setState({ loading: false, data: data });
+    } catch (error) {}
+  };
+
   componentDidUpdate(prevProps, prevState) {
     console.log("5: componentDidUpdate");
     console.log({
@@ -78,6 +55,9 @@ class Badges extends React.Component {
 
   render() {
     console.log("2/4: render");
+    if (this.state.loading === true) {
+      return "Loading...";
+    }
     return (
       <div className="badges">
         <div className="badges__hero">
