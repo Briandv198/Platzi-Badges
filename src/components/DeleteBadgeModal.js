94ab2629c92a6fb15f4d2a27606d35d29ad2ea5c
brian dominguez <briandv198@gmail.com>
2021-06-09 16:30:59 -0500

    :fire: Modal finished

--- src/components/DeleteBadgeModal.js
+++ src/components/DeleteBadgeModal.js
@@ -1,15 +1,19 @@
 import React from "react";
 import Modal from "./Modal";
 
+import "./styles/deleteBadgeModal.css";
+
 import MainButton from "./MainButton";
 
 function DeleteBadgeModal(props) {
   return (
     <Modal isOpen={props.isOpen} onClose={props.onClose}>
-      <h1>Are You Sure?</h1>
-      <p>You are about to delet this badge.</p>
+      <div className="delete-text">
+        <h1>Are You Sure?</h1>
+        <p>You are about to delet this badge.</p>
+      </div>
 
-      <div>
+      <div className="delete-buttons">
         <MainButton
           onClick={props.onDeleteBadge}
           className="delete-button"
