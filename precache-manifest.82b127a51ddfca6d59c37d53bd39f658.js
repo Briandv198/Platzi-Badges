1f9b55f2daf74da34c5eba9856396a0e1df897bf
brian dominguez <briandv198@gmail.com>
2021-06-10 09:27:16 -0500

    Updates

---
+++ precache-manifest.82b127a51ddfca6d59c37d53bd39f658.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8504c76bd17cfb0d24b5",
+    "url": "/Briandv198/Platzi-Badges/static/css/main.bf9680dd.chunk.css"
+  },
+  {
+    "revision": "8504c76bd17cfb0d24b5",
+    "url": "/Briandv198/Platzi-Badges/static/js/main.8504c76b.chunk.js"
+  },
+  {
+    "revision": "de02481134f529660b7f",
+    "url": "/Briandv198/Platzi-Badges/static/js/runtime~main.de024811.js"
+  },
+  {
+    "revision": "13ac72b6442503060ae6",
+    "url": "/Briandv198/Platzi-Badges/static/js/2.13ac72b6.chunk.js"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/Briandv198/Platzi-Badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "decf4bef48802fa3df9424e576c21c4d",
+    "url": "/Briandv198/Platzi-Badges/static/media/error.decf4bef.jpg"
+  },
+  {
+    "revision": "9524a3050dfe1a0d4075c4be701210a7",
+    "url": "/Briandv198/Platzi-Badges/static/media/space1.9524a305.png"
+  },
+  {
+    "revision": "c9f1200836515fd4bacd29160be78732",
+    "url": "/Briandv198/Platzi-Badges/static/media/twitter.c9f12008.svg"
+  },
+  {
+    "revision": "24b05c259647921cf8f51ed0fbabde68",
+    "url": "/Briandv198/Platzi-Badges/index.html"
+  }
+];
